31fd13f02e04dda92423bd0c09ff4d39de1a71fd
jorneylm <jorney95@live.com>
2021-07-21 23:16:15 -0600

    otros cambios

--- src/components/ListaRecetas.js
+++ src/components/ListaRecetas.js
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React, {useContext} from 'react';
+import Receta from './Receta';
+import {RecetasContext} from '../context/RecetasContext';
 
 const ListaRecetas = () => {
+
+    //extraer las recetas
+    const {recetas} = useContext(RecetasContext);
+
     return ( 
-        <h1>Recetas</h1>
+        <div className="row mt-5">
+            {recetas.map(receta=>(
+                <Receta
+                    key={receta.idDrink} 
+                    receta={receta}
+                />
+            ))}
+        </div>
      );
 }
  
